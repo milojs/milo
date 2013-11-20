14fd099d0435caab9fb02abc63c4f1a459d9bb52
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2013-11-20 19:48:32 +0000

    test for Facet class added

--- test/facets/class_spec.js
+++ test/facets/class_spec.js
@@ -1,8 +1,32 @@
+'use strict';
+
 var Facet = require('../../lib/facets/class')
-	, assert = require('assert');
+	, assert = require('assert')
+	, _ = require('proto');
 
 describe('Facet', function() {
-	it('should have init method', function() {
+	it('should initialize Facet objects', function() {
+		var self = {}
+		var aFacet = new Facet(self, { prop: 1 });
+
+		assert.deepEqual(aFacet.owner, self, 'first parameter should be copied to .owner');
+		assert.deepEqual(aFacet.options, { prop: 1 }, 'second parameter should be copied to .options');
+	});
+
+	it('should call init methods of Facet subclasses', function() {
+		var MyFacet = _.createSubclass(Facet, 'MyFacet');
 		
+		assert(MyFacet.prototype instanceof Facet, 'MyFacet is subclass of Facet');
+
+		_.extendProto(MyFacet, {
+			init: function() { 
+				this.initCalled = true;
+			}
+		});
+
+		var self = {}
+			, aFacet = new MyFacet;
+
+		assert.equal(aFacet.initCalled, true, 'init function should be called');
 	});
 });
