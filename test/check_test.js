16c61e5633fc4d10e2b82f95609899f92cb0d470
Jason Green <jasoniangreen@gmail.com>
2013-11-23 19:14:30 +0000

    [Jason] check/match testing and progress on replacement of underscore functions

--- test/check_test.js
+++ test/check_test.js
@@ -18,4 +18,37 @@ describe('check module', function() {
 			check(objFail, Match.ObjectHash(Function));
 		}, 'should fail if one property is a string');
 	});
+
+	var notDefined
+		, nullVar = null
+		, obj = {prop: 'test'}
+		, arr = [1, 2, 3]
+		, func = function(){};
+
+	var toTest = [
+		[func, Function, 'function'],
+		['test', String, 'string'],
+		[8, Number, 'number'],
+		[obj, Object, 'object'],
+		[false, Boolean, 'boolean'],
+		[notDefined, undefined, 'undefined'],
+		[nullVar, null, 'null'],
+		[arr, Array, 'Array']
+	];
+
+	it('should match.test for primitives and other native data types', function() {
+		for (var i = 0; i < toTest.length; i++) {
+			assert(Match.test(toTest[i][0], toTest[i][1]), 'match.test ' + toTest[i][2]);
+			assert(Match.test(toTest[i][0], Match.Any), 'match.test ' + toTest[i][2] + 'with Match.Any');
+		}
+	});
+
+	it('should throw errors or not on check test against primitives and other native data types', function() {
+		for (var i = 0; i < toTest.length; i++) {
+			assert.doesNotThrow(function() {
+				check(toTest[i][0], toTest[i][1]);
+			}, 'check ' + toTest[i][2]);
+		}
+	});
+
 });
