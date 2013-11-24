0e46eb73b8b2863f0eac3d33096c658a712c9c7b
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2013-11-24 21:59:27 +0000

    test fixed, bower.json added

--- test/components/c_facets/cf_registry_test.js
+++ test/components/c_facets/cf_registry_test.js
@@ -4,6 +4,6 @@ var facetsRegistry = require('../../../lib/components/c_facets/cf_registry')
 	, ComponentFacet = require('../../../lib/components/c_facet')
 	, testRegistry = require('../../utils/test_registry');;
 
-describe('registry of components', function() {
+describe('registry of facets', function() {
 	testRegistry(facetsRegistry, ComponentFacet);
 });
