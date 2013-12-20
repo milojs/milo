695f64dc93a4b7c99620d709a6d2b86c6feef8c9
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2013-12-20 12:36:10 +0000

    Facet: domParent, postDomParent methods started

--- milo.bundle.js
+++ milo.bundle.js
@@ -538,7 +538,7 @@ module.exports = classes;
 
 var FacetedObject = require('../facets/f_object')
 	, facetsRegistry = require('./c_facets/cf_registry')
-	, ComponentFacet = require('./c_facet')
+	, ComponentFacet = facetsRegistry.get('ComponentFacet')
 	, Messenger = require('../messenger')
 	, _ = require('mol-proto')
 	, check = require('../util/check')
@@ -799,7 +799,7 @@ function _getScopeParent(withFacet) {
 	}
 }
 
-},{"../config":34,"../facets/f_object":36,"../messenger":40,"../util/check":47,"../util/count":48,"./c_facet":10,"./c_facets/cf_registry":24,"mol-proto":58}],10:[function(require,module,exports){
+},{"../config":34,"../facets/f_object":36,"../messenger":40,"../util/check":47,"../util/count":48,"./c_facets/cf_registry":24,"mol-proto":58}],10:[function(require,module,exports){
 // <a name="components-facet"></a>
 // ###component facet class
 
@@ -817,6 +817,8 @@ function _getScopeParent(withFacet) {
 var Facet = require('../facets/f_class')
 	, Messenger = require('../messenger')
 	, FacetError = require('../util/error').Facet
+	//, componentsRegistry = require('./c_registry')
+	//, Component = componentsRegistry.get('Component')
 	, _ = require('mol-proto');
 
 var ComponentFacet = _.createSubclass(Facet, 'ComponentFacet');
@@ -828,6 +830,8 @@ _.extendProto(ComponentFacet, {
 	init: initComponentFacet,
 	start: startComponentFacet,
 	check: checkDependencies,
+	domParent: domParent,
+	postDomParent: postDomParent,
 	_createMessenger: _createMessenger,
 	_setMessageSource: _setMessageSource,
 	_createMessageSource: _createMessageSource
@@ -862,6 +866,30 @@ function checkDependencies() {
 				throw new FacetError('facet ' + this.constructor.name + ' requires facet ' + reqFacet);
 		}, this);
 	}
+}
+
+
+/**
+ * domParent
+ * @return {ComponentFacet} reference to the facet of the same class of the closest parent DOM element, that has a component with the same facet class attached to it. If such element doesn't exist, will return undefined.
+ */
+function domParent() {
+	return Component.getContainingComponent(this.owner.el, false, this.name);
+	if (parent)
+		parent.toolbar.postMessage('childtoolbar', { visible: visibility });
+}
+
+
+/**
+ * postDomParent
+ * If facet has DOM parent, posts the message to it
+ * @param {String} messageType
+ * @param {Object} messageData
+ */
+function postDomParent(messageType, messageData) {
+	var parent = this.domParent();
+	if (parent)
+		parent[this.name].postMessage(messageType, messageData);
 }
 
 
@@ -2964,6 +2992,7 @@ var _ = require('mol-proto');
 module.exports = Facet;
 
 function Facet(owner, config) {
+	this.name = _.firstLowerCase(this.constructor.name);
 	this.owner = owner;
 	this.config = config || {};
 	this.init.apply(this, arguments);
