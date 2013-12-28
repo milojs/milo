4b78f1ea9cd55cc25cdeb5461c46bb8770104bf8
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2013-12-28 21:17:43 +0000

    added use strict

---
+++ test/messenger/m_source_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var MessageSource = require('../../lib/messenger/m_source')
+	, Messenger = require('../../lib/messenger')
+	, assert = require('assert');
+
+
+describe('MessageSource', function() {
+
+});
