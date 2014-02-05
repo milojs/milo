aeabb469d6cadf6972c94e13bf709bbafef75fb2
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-02-05 00:20:58 +0000

    Component: doxx; "start" method call added to "init" method

--- milo.bundle.js
+++ milo.bundle.js
@@ -952,10 +952,43 @@ var _makeComponentConditionFunc = componentUtils._makeComponentConditionFunc;
 
 /**
  * `milo.Component`
- * Base Component class.
+ * Base Component class. Subclass of [FacetedObject](../abstract/faceted_object), but none of this class methods should be directly used with component.
  * Its constructor passes its parameters, including its [scope](./scope.js.html), DOM element and name to [`init`](#init) method.
  * The constructor of Component class rarely needs to be used directly, as [milo.binder](../binder.js.html) creates components when it scans DOM tree.
  * [`Component.createComponentClass`](#createComponentClass) should be used to create a subclass of Component class with configured facets.
+ *
+ *
+ * ####Component instance properties####
+ *
+ * - el - DOM element that component is attached to. If the second component is attached to the same DOM element, the warning will be logged to console. To get component reference from DOM element use [Component.getComponent](./c_utils.js.html#getComponent) class method. To inspect component via element in browser check `___milo_component` property of element (property name be changed using `milo.config`).
+ * - scope - parent scope object, an instance of [Scope](./scope.js.html) class. To get parent component use [getScopeParent](#Component$getScopeParent) instance method of component. The actual path to get parent of conponent is `component.scope._hostObject.owner`, where `_hostObject` refers to [Container](c_facets/Container.js.html) facet of parent component and `owner` to the parent itself. The children of component are accessible via the scope of its container facet: `component.container.scope`. The scope hierarchy can be the same or different as the DOM hierarchy - DOM children of the component will be on the same scope as component if it does not have `Container` facet and in the scope of its Container facet if it has it. See [Scope](./scope.js.html).
+ * - name - the name of component, should be unique for the scope where component belongs. To find component in scope the component's name should be used as property of scope object. See [Scope](./scope.js.html).
+ * - facets - map of references of all component's facets (facet names are lowercase in this map). All facets can be accessed directly as properties of component, this property can be used to iterate facets (it is used in this way in [allFacets](#Component$allFacets) component's instance method that allows to call method with the same name on all facets).
+ * - extraFacets - an array of names of facets that are added to component and do not form the part of component's class.
+ * - _messenger - the reference to component's [messenger](../messenger/index.js.html). Rarely needs to be used directly as all commonly used methods of mesenger are available directly on component.
+ *
+ *
+ * ####Component events####
+ *
+ * - 'childrenbound' - synchronously dispatched when children of DOM element which compnent is connected to are connected to components. The event is dispatched when component is created with `milo.binder` (as is almost always the case, as all Component class methods that create/copy components use `milo.binder` internally - component constructor and Component.create methods are not used in framework outside of `milo.binder` and rarely if ever need to be used in aplication).
+ * - 'addedtoscope' - synchronously dispatched when component is added to scope.
+ * - 'stateready' - aynchronously dispatched when component (together with its scope children) is created with [Component.createFromState](#Component$$createFromState) (or `createFromDataTransfer`) method. Can be dispatched by application if the component's state is set with some other mechanism. This event is not used in `milo`, it can be used in application in particular subclasses of component.
+ *
+ *
+ * ####Component "lifecycle"####
+ *
+ * 1) Component constructor is called. Component's constructor simply calls constructor of [FacetedObject](../abstract/faceted_object) that is a superclass of Component. Subclasses of Component should not implement their own constructor, they can optionally implement `init` method, but most components do not need to do it.
+ * 2) constructors and `init` methods of all facets are called in sequence. Same as components, facet do not implement their constructors, they can optionally implement `init` and `start` methods (see below). Inside `init` method there should be only general initialization code without any dependency on component itself (it is not ready yet) and other facets (as there is no specific facets creation order). If facet implements `init` method it MUST call inherited init with `ComponentFacet.prototype.init.apply(this, arguments)`.
+ * 3) `init` method of component is called. At this point all facets are created but facets still can be not ready as they can have initialization code in `start` method. If component subclass implements `init` method it MUST call inherited method with `<Superclass>.prototype.init.apply(this, arguments)`, where <Superclass> is Component or another superclass the component is a subclass of.
+ * 4) `check` method of all facets is called. This method adds facets that are not part of the component declaration (being part of the class or explicitely listed in bind attribute) but are required by facets that the compnent already has. Subclasses of [ComponentFacet](./c_facet.js.html) do not need to implement this method.
+ * 5) `start` method of all facets is called. This method is usually implemented by ComponentFacet subclasses and it can have any initialization code that depends on component or on other facets that are the dependencies of a facet. Inherited `start` method should be called int he same way as written above.
+ * 6) `start` method of component is called. This component method can be implemented by subclasses if they need to have some initialization code that depends on some facets and requires that these facets are fully inialized. Often such code also depends on component's scope children as well so this code should be inside `'childrenbound'` event subscriber.
+ * 7) 'addedtoscope' event is dispatched when component is added to its parent's scope or to top level scope created by `milo.binder`.
+ * 7) component's children are created (steps 1-6 above are followed for each child).
+ * 8) 'childrenbound' event is dispatched when all component's children are created and added to their scope (see event description below).
+ * 9) 'stateready' event is dispatched for component and all its children when component is create from state (see event description below).
+ * 10) at this point component is in the "interactive" state when it and its facets will only respond to messages/events that they subscribed to during initialization.
+ *
  *
  * @param {Scope} scope scope to which component will belong. It is usually a top level scope object returned by `milo.binder` or `scope` property of Container facet.
  * @param {Element} element DOM element that component is attached to
@@ -972,13 +1005,15 @@ module.exports = Component;
 /**
  * ####Component class methods####
  *
- * - [createComponentClass](#createComponentClass)
- * - [create](#create)
- * - [copy](#copy)
- * - [createOnElement](#createOnElement)
+ * - [createComponentClass](#Component$$createComponentClass)
+ * - [create](#Component$$create)
+ * - [copy](#Component$$copy)
+ * - [createOnElement](#Component$$createOnElement)
  * - [isComponent](c_utils.js.html#isComponent)
  * - [getComponent](c_utils.js.html#getComponent)
  * - [getContainingComponent](c_utils.js.html#getContainingComponent)
+ * - [createFromState](#Component$$createFromState)
+ * - [createFromDataTransfer](#Component$$createFromDataTransfer)
  */
 _.extend(Component, {
 	createComponentClass: Component$$createComponentClass,
@@ -999,10 +1034,29 @@ delete Component.createFacetedClass;
  *
  * - [init](#Component$init)
  * - [initElement](#Component$initElement)
+ * - [hasFacet](#Component$hasFacet)
  * - [addFacet](#Component$addFacet)
  * - [allFacets](#Component$allFacets)
  * - [remove](#Component$remove)
+ * - [getState](#Component$getState)
+ * - [getTransferState](#Component$getTransferState)
+ * - [setState](#Component$setState)
  * - [getScopeParent](#Component$getScopeParent)
+ * - [getTopScopeParent](#Component$getTopScopeParent)
+ * - [getScopeParentWithClass](#Component$getScopeParentWithClass)
+ * - [getTopScopeParentWithClass](#Component$getTopScopeParentWithClass)
+ * - [walkScopeTree](#Component$walkScopeTree)
+ * - [broadcast](#Component$broadcast)
+ *
+ *
+ * #####[Messenger](../messenger/index.js.html) methods available on component#####
+ *
+ * - [on](../messenger/index.js.html#Messenger$on) - single subscribe
+ * - [off](../messenger/index.js.html#Messenger$off) - single unsubscribe
+ * - [onMessages](../messenger/index.js.html#Messenger$onMessages) - multiple subscribe
+ * - [offMessages](../messenger/index.js.html#Messenger$offMessages) - multiple unsubscribe
+ * - [postMessage](../messenger/index.js.html#Messenger$postMessage) - post message on component
+ * - [getSubscribers](../messenger/index.js.html#Messenger$getSubscribers) - get subscribers for a given message
  */
 _.extendProto(Component, {
 	init: Component$init,
@@ -1301,6 +1355,9 @@ function Component$init(scope, element, name, componentInfo) {
 
 	// start all facets
 	this.allFacets('start');
+
+	// call start method if it's defined in subclass
+	this.start && this.start();
 }
 
 
