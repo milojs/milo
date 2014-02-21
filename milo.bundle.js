7561559358fd42b1ea0125349e30091d9350e363
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-02-21 15:23:04 +0000

    [Evgeny, Manju] added get/set for MLTime to correctly show times

--- milo.bundle.js
+++ milo.bundle.js
@@ -6307,7 +6307,11 @@ var Component = require('../c_class')
 
 var MLTime = Component.createComponentClass('MLTime', {
     events: undefined,
-    data: undefined,
+    data: {
+        get: MLTime_get,
+        set: MLTime_set,
+        del: MLTime_del,
+    },
     dom: {
         cls: 'ml-ui-time'
     }
@@ -6316,6 +6320,45 @@ var MLTime = Component.createComponentClass('MLTime', {
 componentsRegistry.add(MLTime);
 
 module.exports = MLTime;
+
+
+var TIME_REGEX = /^([0-9]{1,2})(?:\:|\.)([0-9]{1,2})$/
+    , TIME_TEMPLATE = 'hh:mm';
+
+function MLTime_get() {
+    var timeStr = this.el.value;
+    var match = timeStr.match(TIME_REGEX);
+    if (! match) return;
+    var hours = match[1]
+        , mins = match[2];
+    if (hours > 23 || mins > 59) return;
+    var time = new Date(1970, 0, 1, hours, mins);
+
+    return _.toDate(time);
+}
+
+
+function MLTime_set(value) {
+    var time = _.toDate(value);
+    if (! time) {
+        this.el.value = '';
+        return;
+    }
+
+    var timeStr = TIME_TEMPLATE
+            .replace('hh', pad(time.getHours()))
+            .replace('mm', pad(time.getMinutes()));
+
+    this.el.value = timeStr;
+    return timeStr;
+
+    function pad(n) {return n < 10 ? '0' + n : n; }
+}
+
+
+function MLTime_del() {
+    this.el.value = '';
+}
 
 },{"../c_class":11,"../c_registry":27}],52:[function(require,module,exports){
 'use strict';
