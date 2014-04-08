c43fb64b3191fbad1258dc452317ad63f40e7ce6
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-04-08 16:14:24 +0100

    MLTextarea resize started

--- milo.bundle.js
+++ milo.bundle.js
@@ -6688,7 +6688,9 @@ module.exports = MLText;
 'use strict';
 
 var Component = require('../c_class')
-    , componentsRegistry = require('../c_registry');
+    , componentsRegistry = require('../c_registry')
+    , _ = require('mol-proto')
+    , logger = require('../../util/logger');
 
 
 var MLTextarea = Component.createComponentClass('MLTextarea', {
@@ -6703,7 +6705,74 @@ componentsRegistry.add(MLTextarea);
 
 module.exports = MLTextarea;
 
-},{"../c_class":11,"../c_registry":27}],53:[function(require,module,exports){
+
+_.extendProto(MLTextarea, {
+    startAutoresize: MLTextarea$startAutoresize,
+    stopAutoresize: MLTextarea$stopAutoresize
+});
+
+
+function MLTextarea$startAutoresize(options) {
+    return;
+    if (this._autoresize)
+        return logger.warn('MLTextarea startAutoresize: autoresize is already on');
+    this._autoresize = true;
+    this._autoresizeOptions = options;
+
+    this.events.on('input', { subscriber: onTextChange, context: this });
+}
+
+
+function onTextChange() {
+    var str = this.el.value
+        , lines = str.split(/\r\n|\r|\n/)
+        , minLines = this._autoresizeOptions.minLines
+        , maxLines = this._autoresizeOptions.maxLines;
+
+    console.log(lines.length, minLines, maxLines);
+
+    if (lines.length >= maxLines)
+        this.el.rows = maxLines;
+    else if (lines.length <= minLines)
+        this.el.rows = minLines;
+    else {
+
+    }
+}
+
+
+function _linesInString(str) {
+    var div = document.createElement('div')
+        , thisStyle = window.getComputedStyle(this.el);
+    _.extend(div.style, {
+        font: thisStyle.font,
+        position: 'absolute',
+        visibility: 'hidden',
+        height: 'auto',
+        width: 'auto'
+    });
+    document.body.appendChild(div);
+    var width = div.offsetWidth
+        , areaWidth = this.el.offsetWidth;
+
+    return Math.floor();
+}
+
+
+function MLTextarea$stopAutoresize() {
+    return;
+    if (! this._autoresize)
+        return logger.warn('MLTextarea stopAutoresize: autoresize is not on');
+}
+
+
+function MLTextarea$destroy() {
+    if (this._autoresize)
+        this.stopAutoresize();
+    Component.prototype.destroy.apply(this, arguments);
+}
+
+},{"../../util/logger":90,"../c_class":11,"../c_registry":27,"mol-proto":101}],53:[function(require,module,exports){
 'use strict';
 
 var Component = require('../c_class')
