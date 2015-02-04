f7e580d8fb02a73429950d4751c733140c2948d7
Richard Walton <richard.walton@mailonline.co.uk>
2015-02-04 09:45:19 +0000

    Added workaround function for performance issues in select component.

--- milo.bundle.js
+++ milo.bundle.js
@@ -7697,7 +7697,7 @@ var MLSelect = Component.createComponentClass('MLSelect', {
     events: undefined,
     model: {
         messages: {
-            '***': { subscriber: onOptionsChange, context: 'owner' }
+            '**': { subscriber: onOptionsChange, context: 'owner' }
         }
     },
     template: {
@@ -7714,8 +7714,21 @@ module.exports = MLSelect;
 
 
 _.extendProto(MLSelect, {
+    setOptions: MLSelect$setOptions,
     disable: MLSelect$disable
 });
+
+
+function MLSelect$setOptions(options) {
+    // Set options temporarily disables model subscriptions (As a workaround for performance issues relating to model updates / template re-rendering)
+    var modelChangeListener = { context: this, subscriber: onOptionsChange };
+
+    this.model.off('**', modelChangeListener);
+    this.model.set(options);
+    this.model.on('**', modelChangeListener);
+
+    onOptionsChange.call(this);
+}
 
 
 function MLSelect$disable(disable) {
