9f5151f99813cf09a0f46517d339f27ee26e08ec
Evgeny Poberezkin <e.poberezkin@me.com>
2015-06-05 10:12:55 +0100

    zeroTimeout tests

--- test_browser/util/zero_timeout.js
+++ test_browser/util/zero_timeout.js
@@ -5,13 +5,42 @@ var zeroTimeout = require('../../lib/util/zero_timeout')
     , assert = require('assert');
 
 
-describe('util.zeroTimeout', function() {
+describe.only('util.zeroTimeout', function() {
     it('should schedule a delayed execution', function (done) {
         var executed;
         function execute() { executed = true; }
         zeroTimeout(execute);
-        _.delay(function() {
+        setTimeout(function() {
             assert(executed);
+            done();
+        }, 10)
+    });
+
+
+    it('should schedule multiple delayed executions', function (done) {
+        var executed1, executed2;
+        function execute1() { executed1 = true; }
+        function execute2() { executed2 = true; }
+        zeroTimeout(execute1);
+        zeroTimeout(execute2);
+        setTimeout(function() {
+            assert(executed1);
+            assert(executed2);
+            done();
+        }, 10)
+    });
+
+
+    it('should clear delayed executions', function (done) {
+        var executed1, executed2;
+        function execute1() { executed1 = true; }
+        function execute2() { executed2 = true; }
+        var id = zeroTimeout(execute1);
+        zeroTimeout(execute2);
+        zeroTimeout.clear(id)
+        setTimeout(function() {
+            assert(!executed1);
+            assert(executed2);
             done();
         }, 10)
     });
