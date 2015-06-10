5a4579d06b37a25254bf002d81a53e7bc825f7d3
Evgeny Poberezkin <e.poberezkin@me.com>
2015-06-10 17:14:21 +0100

    completed zero-timeout - doesnt really improve performance

--- test_browser/util/zero_timeout.js
+++ test_browser/util/zero_timeout.js
@@ -1,15 +1,14 @@
 'use strict';
 
 
-var zeroTimeout = require('../../lib/util/zero_timeout')
-    , assert = require('assert');
+var assert = require('assert');
 
 
-describe.only('util.zeroTimeout', function() {
+describe('util.zeroTimeout', function() {
     it('should schedule a delayed execution', function (done) {
         var executed;
         function execute() { executed = true; }
-        zeroTimeout(execute);
+        milo.util.zeroTimeout(execute);
         setTimeout(function() {
             assert(executed);
             done();
@@ -21,25 +20,10 @@ describe.only('util.zeroTimeout', function() {
         var executed1, executed2;
         function execute1() { executed1 = true; }
         function execute2() { executed2 = true; }
-        zeroTimeout(execute1);
-        zeroTimeout(execute2);
+        milo.util.zeroTimeout(execute1);
+        milo.util.zeroTimeout(execute2);
         setTimeout(function() {
             assert(executed1);
-            assert(executed2);
-            done();
-        }, 10)
-    });
-
-
-    it('should clear delayed executions', function (done) {
-        var executed1, executed2;
-        function execute1() { executed1 = true; }
-        function execute2() { executed2 = true; }
-        var id = zeroTimeout(execute1);
-        zeroTimeout(execute2);
-        zeroTimeout.clear(id)
-        setTimeout(function() {
-            assert(!executed1);
             assert(executed2);
             done();
         }, 10)
