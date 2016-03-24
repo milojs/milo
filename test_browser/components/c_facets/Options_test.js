964a4d3a75e610ba5c2bca7dc6fc7e3ca1bf83d9
Jason Green <jasoniangreen@gmail.com>
2016-03-24 23:09:20 +0000

    Need to clone object when extending with ml-options values

--- test_browser/components/c_facets/Options_test.js
+++ test_browser/components/c_facets/Options_test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var assert = require('assert');
-var HTML = '<div ml-bind="OptionsComponent:comp" ml-options="foo=bar&test=test321"></div>';
+var HTML = '<div ml-bind="OptionsComponent:comp" ml-options="foo=bar&test=test321"></div>'
+         + '<div ml-bind="OptionsComponent:comp2"></div>';
 
 describe.only('Options facet', function() {
     milo.config.check = true; // Enable 'check' library so that inputs to the Css facet are validated
@@ -19,6 +20,7 @@ describe.only('Options facet', function() {
 
     var scope;
     var component;
+    var component2;
 
     beforeEach(function() {
         var element = document.createElement('div');
@@ -27,9 +29,11 @@ describe.only('Options facet', function() {
         // Bind the element
         scope = milo.binder(element);
         component = scope.comp;
+        component2 = scope.comp2;
     });
 
     it('options attr values should extend component definition values', function() {
         assert.deepEqual(component.options.m.get(), { test: 'test321', foo: 'bar' });
+        assert.deepEqual(component2.options.m.get(), { test: 'test123' });
     });
 });
