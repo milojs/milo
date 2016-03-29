571436dbbdaf41f6ca7c57c1c18c36a9a3b27086
Jason Green <jasoniangreen@gmail.com>
2016-03-29 10:36:24 +0000

    Added coerceTypes config option to options facet for use with ml-options attr. Only works with bools and numbers.

--- test_browser/components/c_facets/Options_test.js
+++ test_browser/components/c_facets/Options_test.js
@@ -4,7 +4,9 @@ var assert = require('assert');
 var HTML = '<div ml-bind="OptionsComponent:comp" ml-options="foo=bar&test=test321"></div>'
          + '<div ml-bind="OptionsComponent:comp2"></div>';
 
-describe.only('Options facet', function() {
+var HTML_COERCE = '<div ml-bind="OptionsComponentCoerce:comp" ml-options="bool1=true&bool2=false&num=34&float1=34.5&float2=0.034&addr=32 green st"></div>'
+
+describe('Options facet', function() {
     milo.config.check = true; // Enable 'check' library so that inputs to the Css facet are validated
 
     milo.createComponentClass({
@@ -18,22 +20,45 @@ describe.only('Options facet', function() {
         }
     });
 
-    var scope;
-    var component;
-    var component2;
+    milo.createComponentClass({
+        className: 'OptionsComponentCoerce',
+        facets: {
+            options: {
+                coerceTypes: true
+            }
+        }
+    });
 
-    beforeEach(function() {
+    it('options attr values should extend component definition values', function() {
         var element = document.createElement('div');
         element.innerHTML = HTML;
 
         // Bind the element
-        scope = milo.binder(element);
-        component = scope.comp;
-        component2 = scope.comp2;
-    });
+        var scope = milo.binder(element);
+        var component = scope.comp;
+        var component2 = scope.comp2;
 
-    it('options attr values should extend component definition values', function() {
         assert.deepEqual(component.options.m.get(), { test: 'test321', foo: 'bar' });
         assert.deepEqual(component2.options.m.get(), { test: 'test123' });
     });
+
+    it('coerce types config should make implicit type coersions', function() {
+        var element = document.createElement('div');
+        element.innerHTML = HTML_COERCE;
+
+        // Bind the element
+        var scope = milo.binder(element);
+        var component = scope.comp;
+
+        assert.deepEqual(component.options.m.get(), {
+            bool1: true,
+            bool2: false,
+            num: 34,
+            float1: 34.5,
+            float2: 0.034,
+            addr: '32 green st'
+        });
+    });
+
+
 });
